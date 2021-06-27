aa26d046a7a41c9feec51c32ceb6bb3fea65f15c
learnitaway <shashikalaraju100@gmail.com>
2021-06-27 23:51:33 +0530

    Add files via upload

---
+++ StaticPlot.py
@@ -0,0 +1,13 @@
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+x_vals=[0,1,2,3,4,5]
+y_vals=[0, 1, 3, 2, 3, 5, ]
+
+plt.plot(x_vals, y_vals)
+plt.tight_layout()
+plt.show()
